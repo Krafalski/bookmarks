217ae1f8c20c3d6f21912a438e674a580fc0b345
Karolin Rafalski <kr2174@caa.columbia.edu>
2016-07-16 19:08:51 -0400

    updated alert messges and overall appearance

--- public/html/bookmarklist.js
+++ public/html/bookmarklist.js
@@ -3,7 +3,7 @@ var html            = require ('./html.js');
 function bookmarkList(row, j){
   var site    ='';
   site += '<tr class="each-site">';
-  site += '<td><a target="_blank" href="' + row[j].url + '">' + row[j].title +'</a></td>'
+  site += '<td class="title"><a target="_blank" href="' + row[j].url + '">' + row[j].title +'</a></td>'
   site += '<td> </td> '
   site += '<td><a href="delete/' + row[j].id + '" class="delete"> x </a></td>'
   site += '</tr>';
